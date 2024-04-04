2d7d09f3f2bb4f1647c95e64e657c4bc6f68e413
Cris <c.cheuquel01@ufromail.cl>
2024-04-04 16:16:18 -0300

    main.leerCadena() created: read a string.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
+import java.util.Scanner;
 public class Main {
+    public static Scanner leer = new Scanner(System.in);
+
     public static void main(String[] args) {
 
 
+    }
+    public static String leerCadena(String cadena){
+        return leer.nextLine();
     }
 }
