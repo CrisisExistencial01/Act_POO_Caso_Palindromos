6bced873fc501ec34b6827177d9b5b12e9a0d064
Cris <c.cheuquel01@ufromail.cl>
2024-04-04 17:25:44 -0300

    Documentation added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,14 +10,21 @@ public class Main {
         return leer.nextLine();
     }
     public static String borrarEspacios(String cadena){
+        /*
+         * reemplaza las coincidencias de espacios en blanco por "",
+         * \\s+ son dichos espacios
+         */
         return cadena.replaceAll("\\s+", "");
     }
     public static String invertirString(String cadena){
+        // Crea un objeto StringBuilder y usa StringBuilder.reverse()
         StringBuilder tmp = new StringBuilder(cadena);
         return tmp.reverse().toString();
     }
     public static boolean esPalindromo(String cadena){
-        String inversa = invertirString(borrarEspacios(cadena));
+        // borra los espacios e invierte la string
+        String inversa = invertirString( borrarEspacios( cadena ) );
+        //compara la string original con la invertida ignorando mayusculas
         return cadena.equalsIgnoreCase(inversa);
     }
 }
